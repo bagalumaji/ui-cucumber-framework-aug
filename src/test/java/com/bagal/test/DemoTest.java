99de0f2ce1562462f64d1bf01f1695d2cf84c7ec
Umaji Bagal <bagalumaji@gmail.com>
2024-09-22 10:41:30 +0530

    updated tests

--- src/test/java/com/bagal/test/DemoTest.java
+++ src/test/java/com/bagal/test/DemoTest.java
@@ -1,12 +1,13 @@
 package com.bagal.test;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 public class DemoTest {
+
     @Test
-    void demoTest(){
+    public void demoTest(){
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
