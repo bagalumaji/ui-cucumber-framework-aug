25acad0bd982dec55a115e2af9ad758ce28a2b19
umaji bagal <bagalumaji@gmail.com>
2024-10-26 22:13:36 +0530

    added CucumberTestNGRunnerTest

---
+++ src/test/java/com/bagal/runner/CucumberTestNGRunnerTest.java
@@ -0,0 +1,8 @@
+package com.bagal.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/features",glue = {"com.bagal.stepDefinitions"})
+public class CucumberTestNGRunnerTest extends AbstractTestNGCucumberTests {
+}
