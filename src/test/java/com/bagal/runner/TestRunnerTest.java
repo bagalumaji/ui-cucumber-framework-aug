a5c7c0db707f4e24f8e1c67f47ac7fe5195998c3
umaji bagal <bagalumaji@gmail.com>
2024-10-26 22:16:22 +0530

    refactored name

--- src/test/java/com/bagal/runner/TestRunnerTest.java
+++ src/test/java/com/bagal/runner/TestRunnerTest.java
@@ -4,5 +4,5 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",glue = {"com.bagal.stepDefinitions"})
-public class CucumberTestNGRunnerTest extends AbstractTestNGCucumberTests {
+public class TestRunnerTest extends AbstractTestNGCucumberTests {
 }
