09056e49fda4371c8cb46bade3e537bd9e1a5c2e
umaji bagal <bagalumaji@gmail.com>
2024-10-27 08:53:05 +0530

    added selenium code

--- src/test/java/com/bagal/stepDefinitions/LoginSteps.java
+++ src/test/java/com/bagal/stepDefinitions/LoginSteps.java
@@ -4,11 +4,17 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginSteps {
+    WebDriver driver;
     @Given("the user is on the login page")
     public void theUserIsOnTheLoginPage() {
         System.out.println("The user is on the login page");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     }
 
     @When("the user enters {string} as the username and {string} as the password")
