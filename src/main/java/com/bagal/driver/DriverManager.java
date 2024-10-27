8dd3069b9eebed50c0d3537af65c1fffff530dab
umaji bagal <bagalumaji@gmail.com>
2024-10-27 08:58:23 +0530

    added setdriver method

--- src/main/java/com/bagal/driver/DriverManager.java
+++ src/main/java/com/bagal/driver/DriverManager.java
@@ -1,4 +1,16 @@
 package com.bagal.driver;
 
-public class DriverManager {
+public final class DriverManager {
+    private DriverManager() {
+    }
+
+    static void setDriver() {
+
+    }
+
+    public static void getDriver() {
+    }
+
+    public static void unloadDriver() {
+    }
 }
