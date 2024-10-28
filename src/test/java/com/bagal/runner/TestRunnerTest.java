16d1d19683be9837c6cd63448b6a5da847c105be
umaji bagal <bagalumaji@gmail.com>
2024-10-28 08:42:03 +0530

    updated test runner

--- src/test/java/com/bagal/runner/TestRunnerTest.java
+++ src/test/java/com/bagal/runner/TestRunnerTest.java
@@ -3,6 +3,6 @@ package com.bagal.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features",glue = {"com.bagal.stepDefinitions"})
+@CucumberOptions(features = "src/test/resources/features",glue = {"com.bagal.stepDefinitions","com.bagal.hooks"})
 public class TestRunnerTest extends AbstractTestNGCucumberTests {
 }
