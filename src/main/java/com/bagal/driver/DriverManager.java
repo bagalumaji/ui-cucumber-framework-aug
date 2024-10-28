98df270e5929a467d1a3f9df1e424507e30c04e1
umaji bagal <bagalumaji@gmail.com>
2024-10-28 08:40:45 +0530

    updated DriverManager

--- src/main/java/com/bagal/driver/DriverManager.java
+++ src/main/java/com/bagal/driver/DriverManager.java
@@ -1,16 +1,20 @@
 package com.bagal.driver;
 
+import org.openqa.selenium.WebDriver;
+
 public final class DriverManager {
-    private DriverManager() {
+    private DriverManager() {}
+    private static final ThreadLocal<WebDriver> WEB_DRIVER_THREAD_LOCAL = new ThreadLocal<>();
+
+    static void setDriver(WebDriver driver) {
+        WEB_DRIVER_THREAD_LOCAL.set(driver);
     }
 
-    static void setDriver() {
-
-    }
-
-    public static void getDriver() {
+    public static WebDriver getDriver() {
+        return WEB_DRIVER_THREAD_LOCAL.get();
     }
 
     public static void unloadDriver() {
+        WEB_DRIVER_THREAD_LOCAL.remove();
     }
 }
