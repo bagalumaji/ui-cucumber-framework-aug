cc59c9c0c3e989852486ceea612a561ac7fc94db
umaji bagal <bagalumaji@gmail.com>
2024-10-28 08:41:03 +0530

    created IConfig

--- src/main/java/com/bagal/config/IConfig.java
+++ src/main/java/com/bagal/config/IConfig.java
@@ -2,6 +2,12 @@ package com.bagal.config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "system:env",
+        "file://${user.dir}/src/test/resources/config.properties"
+})
 public interface IConfig extends Config {
     String url();
 }
