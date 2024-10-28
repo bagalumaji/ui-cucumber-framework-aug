e4e5502fa1b7590cd15a9a3ff93ae28fed9a3d7c
umaji bagal <bagalumaji@gmail.com>
2024-10-28 08:40:18 +0530

    updated Driver

--- src/main/java/com/bagal/driver/Driver.java
+++ src/main/java/com/bagal/driver/Driver.java
@@ -1,7 +1,25 @@
 package com.bagal.driver;
 
+import com.bagal.config.ConfigReader;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Objects;
+
 public final class Driver {
     private Driver() {}
-    public static void initializeDriver() {}
-    public static void closeDriver() {}
+    public static void initializeDriver() {
+        if(Objects.isNull(DriverManager.getDriver())) {
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get(ConfigReader.getConfig().url());
+            DriverManager.setDriver(driver);
+        }
+    }
+    public static void closeDriver() {
+        if(Objects.nonNull(DriverManager.getDriver())) {
+            DriverManager.getDriver().quit();
+            DriverManager.unloadDriver();
+        }
+    }
 }
