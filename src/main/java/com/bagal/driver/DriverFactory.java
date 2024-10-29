338487f3ad11c0c352f7b9b58a2e2058deadf3eb
umaji bagal <bagalumaji@gmail.com>
2024-10-29 08:36:52 +0530

    added code for localdriver factory

--- src/main/java/com/bagal/driver/DriverFactory.java
+++ src/main/java/com/bagal/driver/DriverFactory.java
@@ -1,6 +1,21 @@
 package com.bagal.driver;
 
+import com.bagal.driver.factory.LocalChromeManager;
+import com.bagal.driver.factory.LocalFirefoxManager;
+import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public final class DriverFactory {
     private DriverFactory() {}
-
+    private static Map<String, Supplier<WebDriver>> MAP = new HashMap<>();
+    static {
+        MAP.put("CHROME", LocalChromeManager::getDriver);
+        MAP.put("FIREFOX", LocalFirefoxManager::getDriver);
+    }
+    public static WebDriver getDriver(String browser) {
+        return MAP.get(browser.toUpperCase()).get();
+    }
 }
