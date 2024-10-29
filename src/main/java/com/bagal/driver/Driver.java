5dda9ffee9527822c80621cbcfbcf6b35bb58124
umaji bagal <bagalumaji@gmail.com>
2024-10-29 08:36:36 +0530

    updated driver class

--- src/main/java/com/bagal/driver/Driver.java
+++ src/main/java/com/bagal/driver/Driver.java
@@ -2,7 +2,6 @@ package com.bagal.driver;
 
 import com.bagal.config.ConfigReader;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.Objects;
 
@@ -10,7 +9,7 @@ public final class Driver {
     private Driver() {}
     public static void initializeDriver() {
         if(Objects.isNull(DriverManager.getDriver())) {
-            WebDriver driver = new ChromeDriver();
+            WebDriver driver = DriverFactory.getDriver(ConfigReader.getConfig().browser());
             driver.manage().window().maximize();
             driver.get(ConfigReader.getConfig().url());
             DriverManager.setDriver(driver);
